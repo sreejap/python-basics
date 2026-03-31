89326c2d142788efe4d53eb6936d45b1e892f2cd
sreejap <sreeja605@gmail.com>
2026-03-30 21:36:58 -0400

    Add comments to clarify matrix copying
    # mat is the matrix provided
    # creates a copy of mat, specifically a shallow copy of a 2D list (matrix).

--- Arrays/InitMatrix.py
+++ Arrays/InitMatrix.py
@@ -1,1 +1,4 @@
-matrix = [row[:] for row in mat] # mat is the matrix provided
+matrix = [row[:] for row in mat] 
+
+# mat is the matrix provided
+# creates a copy of mat, specifically a shallow copy of a 2D list (matrix).
